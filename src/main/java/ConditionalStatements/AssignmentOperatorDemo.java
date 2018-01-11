ba25a99000ae03f065b85008c258cb7dcb9f651b
Sandeep <gaddamsandeepreddy123@gmail.com>
2018-01-11 11:41:32 +0530

    commiting

---
+++ src/main/java/ConditionalStatements/AssignmentOperatorDemo.java
@@ -0,0 +1,31 @@
+package ConditionalStatements;
+
+public class AssignmentOperatorDemo {
+
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+
+			      int num1 = 10;
+			      int num2 = 20;
+			      int num3 = 99;
+
+			      num2 = num1;
+			      System.out.println("= Output: "+num2);
+
+			      num2 += num1;
+			      System.out.println("+= Output: "+num2);
+				      
+			      num2 -= num1;
+			      System.out.println("-= Output: "+num2);
+				      
+			      num2 *= num1;
+			      System.out.println("*= Output: "+num2);
+				      
+			      num2 /= num1;
+			      System.out.println("/= Output: "+num2);
+				      
+			      num2 %= num1;
+			      System.out.println("%= Output: "+num2);
+			   }
+			}
+}}}
